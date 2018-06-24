688bebf3c16f5c8655b4759fd12e6a59414c2004
Mangirish Dalvi <mdalvi1@binghamton.edu>
2018-06-23 21:56:04 -0400

    Minor changes

--- src/App.js
+++ src/App.js
@@ -113,9 +113,7 @@ class App extends Component {
 
 	render() {
 		let {errors}=this.state;
-		let container=(errors)?(
-									<div className="current"><span id="error">{errors}</span></div>
-								)	
+		let container=(errors)?(<div className="current"><span id="error">{errors}</span></div>)	
 							  :(this.state.loading===true)?null
 							  :(this.state.selected==='daily')?(
 									<ForeCastList 
